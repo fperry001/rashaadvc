2568921650390421abab3930883fd73701ce3b63
Felicia Perry <perryagencyservices@gmail.com>
2018-01-18 23:09:21 -0500

    final code

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,16 @@
+export default function(){
+	return[
+	{title: 'About', 
+	pages: "Rashaad Holley wants the number 25 to symbolize a rebirth of black males.Holley’s brand, Vingt Cinq, means 25 in French and represents an important age for black men in the U.S., due to inner-city violence.“There’s a popular Kanye lyric, ‘I wasn’t supposed to make it past 25, guess what, I’m still alive’ and that stuck with me,” Holley, a Buffalo State student and UB alumnus, said. “There are no fashion designers coming from the part of Buffalo where I grew up. Me aspiring to be one is different, it’s not something that kids think about being.”Holley graduated from UB in 2016 with a degree in business focused in marketing. Instead of entering the workforce, he decided to pursue a second degree in apparel design within the Fashion and Textile Technology department at Buffalo State.Holley’s goal is to “bring luxury to the urban look.” He’s taken the roughness of the eastside of Buffalo and uses it as inspiration for his clothing line. His great aunt’s death and his distance from his birth father showed him the importance of familial bonds and guided him through his work.",
+	src:'../style/images/think.jpg'},
+	{title: 'Vinct', 
+	pages: "Holley’s goal is to “bring luxury to the urban look.” He’s taken the roughness of the eastside of Buffalo and uses it as inspiration for his clothing line. His great aunt’s death and his distance from his birth father showed him the importance of familial bonds and guided him through his work.",
+	src:'../style/images/vinct.jpg'},
+	{title: 'Shop', 
+	pages: "Shop internationally coming soon",
+	src:'../style/images/group.jpg'},
+	{title: 'Contact', 
+	pages: "Currently in Milan, Italy...please contact via social media",
+	src:'../style/images/stance.jpg'}
+	]
+}
